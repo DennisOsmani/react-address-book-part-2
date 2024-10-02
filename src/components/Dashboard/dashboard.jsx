6ea7a2a09c67ccec0158ae3aac694926d707a44d
Dennis Rizah Nordvi Osmani <dennis.osmani@no.experis.com>
2024-10-02 15:53:59 +0200

    some css added

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,5 +1,3 @@
-import { useContext } from "react";
-import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 
 export default function Dashboard() {
@@ -10,11 +8,8 @@ export default function Dashboard() {
         <>
         <div className="menu">
             <h2>Menu</h2>
-            <Link to="/contacts">Contact List</Link>
+            <Link className="topLink" to="/contacts">Contact List</Link>
             <Link to="/create">Create New Contact</Link>
-        </div>
-        <div className="part2">
-
         </div>
         </>
     );
